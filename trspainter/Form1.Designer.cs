c3fe98b881d48e5c892cffe6ad0af0d771b3ee62
charlierobson <retrogit@gmail.com>
2016-06-17 12:18:43 +0100

    initial commit

---
+++ trspainter/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace trspainter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fillToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.invertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelSubPix = new System.Windows.Forms.Label();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hatchedBackgroundToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.greenScreenVDUToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelCharCell = new System.Windows.Forms.Label();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(869, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.loadToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.quitToolStripMenuItem.Text = "Exit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem,
+            this.fillToolStripMenuItem,
+            this.invertToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // fillToolStripMenuItem
+            // 
+            this.fillToolStripMenuItem.Name = "fillToolStripMenuItem";
+            this.fillToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.fillToolStripMenuItem.Text = "Fill";
+            this.fillToolStripMenuItem.Click += new System.EventHandler(this.fillToolStripMenuItem_Click);
+            // 
+            // invertToolStripMenuItem
+            // 
+            this.invertToolStripMenuItem.Name = "invertToolStripMenuItem";
+            this.invertToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.invertToolStripMenuItem.Text = "Invert";
+            this.invertToolStripMenuItem.Click += new System.EventHandler(this.invertToolStripMenuItem_Click);
+            // 
+            // labelSubPix
+            // 
+            this.labelSubPix.Location = new System.Drawing.Point(798, 1);
+            this.labelSubPix.Name = "labelSubPix";
+            this.labelSubPix.Size = new System.Drawing.Size(71, 23);
+            this.labelSubPix.TabIndex = 1;
+            this.labelSubPix.Text = "0,0";
+            this.labelSubPix.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hatchedBackgroundToolStripMenuItem,
+            this.greenScreenVDUToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // hatchedBackgroundToolStripMenuItem
+            // 
+            this.hatchedBackgroundToolStripMenuItem.CheckOnClick = true;
+            this.hatchedBackgroundToolStripMenuItem.Name = "hatchedBackgroundToolStripMenuItem";
+            this.hatchedBackgroundToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.hatchedBackgroundToolStripMenuItem.Text = "Hatched background";
+            this.hatchedBackgroundToolStripMenuItem.Click += new System.EventHandler(this.hatchedBackgroundToolStripMenuItem_Click);
+            // 
+            // greenScreenVDUToolStripMenuItem
+            // 
+            this.greenScreenVDUToolStripMenuItem.CheckOnClick = true;
+            this.greenScreenVDUToolStripMenuItem.Name = "greenScreenVDUToolStripMenuItem";
+            this.greenScreenVDUToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.greenScreenVDUToolStripMenuItem.Text = "Green Screen VDU";
+            this.greenScreenVDUToolStripMenuItem.Click += new System.EventHandler(this.greenScreenVDUToolStripMenuItem_Click);
+            // 
+            // labelCharCell
+            // 
+            this.labelCharCell.Location = new System.Drawing.Point(721, 1);
+            this.labelCharCell.Name = "labelCharCell";
+            this.labelCharCell.Size = new System.Drawing.Size(71, 23);
+            this.labelCharCell.TabIndex = 2;
+            this.labelCharCell.Text = "0,0";
+            this.labelCharCell.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(149, 6);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 579);
+            this.Controls.Add(this.labelCharCell);
+            this.Controls.Add(this.labelSubPix);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "TRS Pixel Graphic Designer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fillToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem invertToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.Label labelSubPix;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hatchedBackgroundToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem greenScreenVDUToolStripMenuItem;
+        private System.Windows.Forms.Label labelCharCell;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+    }
+}
+
---
+++ trspainter/Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace trspainter
+{
+    public partial class Form1 : Form
+    {
+        private float _grDx, _grDy;
+        private int _grXCell, _grYCell;
+
+        private float _chDx, _chDy;
+        private int _chXCell, _chYCell;
+
+        private Bitmap _canvas;
+
+        private bool _drawing, _erasing;
+
+        private readonly bool[,] _pixels;
+
+        private bool _greenScreen;
+        private bool _solidBgMode = true;
+
+        private readonly Brush _darkBrush;
+        private readonly Brush _slightlyLessDarkBrush;
+        private bool _changes;
+
+        private string _currentFile;
+
+        private string CurrentFile
+        {
+            get { return _currentFile; }
+
+            set
+            {
+                _currentFile = value;
+
+                Text = @"TRS Pixel Graphic Designer";
+                if (!string.IsNullOrWhiteSpace(_currentFile))
+                {
+                    Text += $" : {_currentFile}";
+                }
+
+                if (_changes)
+                {
+                    Text += @" *";
+                }
+            }
+        }
+
+        public Form1()
+        {
+            _darkBrush = new SolidBrush(Color.FromArgb(255, 40, 40, 40));
+            _slightlyLessDarkBrush = new SolidBrush(Color.FromArgb(255, 50, 50, 50));
+
+            _pixels = new bool[128, 48];
+
+            InitializeComponent();
+            OnResizeHandler();
+        }
+
+        private int ClientWidthAdjusted => ClientSize.Width;
+        private int ClientHeightAdjusted => ClientSize.Height - menuStrip1.Height;
+
+        private void OnResizeHandler()
+        {
+            _grDx = (float)ClientWidthAdjusted / 128;
+            _grDy = (float)ClientHeightAdjusted / 48;
+
+            _chDx = (float)ClientWidthAdjusted / 64;
+            _chDy = (float)ClientHeightAdjusted / 16;
+
+            _canvas = new Bitmap(ClientWidthAdjusted, ClientHeightAdjusted);
+
+            var g = Graphics.FromImage(_canvas);
+
+            for (var y = 0; y < 48; ++y)
+            {
+                for (var x = 0; x < 128; ++x)
+                {
+                    var hatch = (((y/3) & 1) ^ ((x/2) & 1)) == 0;
+                    hatch |= _solidBgMode;
+
+                    g.FillRectangle(_pixels[x,y] ? _greenScreen ? Brushes.LawnGreen : Brushes.WhiteSmoke : hatch ? _darkBrush: _slightlyLessDarkBrush, x*_grDx, y*_grDy, _grDx, _grDy);
+                }
+            }
+
+            Invalidate();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            OnResizeHandler();
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            var pos = e.Location;
+
+            var cxCell = (int)Math.Floor(pos.X / _grDx);
+            var cyCell = (int)Math.Floor((pos.Y - menuStrip1.Height) / _grDy);
+
+            _chXCell = (int)Math.Floor(pos.X / _chDx);
+            _chYCell = (int)Math.Floor((pos.Y - menuStrip1.Height) / _chDy);
+
+            labelCharCell.Text = $"{_chXCell},{_chYCell}";
+            labelSubPix.Text = $"{_grXCell},{_grYCell}";
+
+            if (cxCell == _grXCell && cyCell == _grYCell) return;
+
+            _grXCell = cxCell;
+            _grYCell = cyCell;
+
+            if (_drawing)
+            {
+                DrawCell();
+            }
+            Invalidate();
+        }
+
+        private void DrawCell()
+        {
+            if (_grXCell < 0 || _grXCell > 127 || _grYCell < 0 || _grYCell > 48) return;
+
+            _pixels[_grXCell, _grYCell] = _erasing == false;
+
+            var hatch = ((_chYCell & 1) ^ (_chXCell & 1)) == 0;
+            hatch |= _solidBgMode;
+
+            Graphics.FromImage(_canvas)
+                .FillRectangle(_pixels[_grXCell, _grYCell] ? _greenScreen ? Brushes.LawnGreen : Brushes.WhiteSmoke : (hatch ? _darkBrush : _slightlyLessDarkBrush), _grXCell * _grDx, _grYCell * _grDy, _grDx, _grDy);
+
+            Invalidate();
+
+            _changes = true;
+            CurrentFile = CurrentFile; // to put the '*' on the name
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+
+            _erasing = _pixels[_grXCell, _grYCell];
+            _drawing = true;
+
+            DrawCell();
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            _erasing = false;
+            _drawing = false;
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(_canvas, 0, menuStrip1.Height);
+        }
+
+        private void DoSaveToCurrentFile()
+        {
+            var total = 0;
+            var output = new StringBuilder();
+
+            for (var y = 0; y < 48; y += 3)
+            {
+                for (var x = 0; x < 128; x += 2)
+                {
+                    var c = 0;
+
+                    if (_pixels[x, y]) c |= 1;
+                    if (_pixels[x + 1, y]) c |= 2;
+
+                    if (_pixels[x, y + 1]) c |= 4;
+                    if (_pixels[x + 1, y + 1]) c |= 8;
+
+                    if (_pixels[x, y + 2]) c |= 16;
+                    if (_pixels[x + 1, y + 2]) c |= 32;
+
+                    output.Append($"{c + 128}");
+
+                    ++total;
+                    output.Append((total & 63) == 0 ? "\n" : ", ");
+                }
+            }
+
+            File.WriteAllText(CurrentFile, output.ToString());
+
+            Change(false);
+        }
+
+        private void Change(bool changes)
+        {
+            _changes = changes;
+            CurrentFile = CurrentFile;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (CurrentFile == null)
+            {
+                var s = new SaveFileDialog();
+                var result = s.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    CurrentFile = s.FileName;
+                }
+                else return;
+            }
+            DoSaveToCurrentFile();
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var s = new SaveFileDialog();
+            var result = s.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                CurrentFile = s.FileName;
+            }
+            else return;
+
+            DoSaveToCurrentFile();
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var o = new OpenFileDialog();;
+            var result = o.ShowDialog();
+            if (result != DialogResult.OK) return;
+
+            Array.Clear(_pixels, 0, _pixels.Length);
+
+            var input = File.ReadAllLines(o.FileName);
+
+            CurrentFile = o.FileName;
+
+            var ints = new int[64*16];
+            var splits = new[] { ',' };
+
+            var c = 0;
+            foreach (var n in from line in input select line.Trim().Split(splits, StringSplitOptions.RemoveEmptyEntries) into elements from element in elements select int.Parse(element))
+            {
+                ints[c] = n;
+                ++c;
+            }
+
+            c = 0;
+            foreach (var n in ints)
+            {
+                var x = (c & 63) * 2;
+                var y = (c / 64) * 3;
+                ++c;
+
+                _pixels[x, y] = (n & 1) != 0;
+                _pixels[x + 1, y] = (n & 2) != 0;
+                _pixels[x, y + 1] = (n & 4) != 0;
+                _pixels[x + 1, y + 1] = (n & 8) != 0;
+                _pixels[x, y + 2] = (n & 16) != 0;
+                _pixels[x + 1, y + 2] = (n & 32) != 0;
+            }
+
+            OnResizeHandler();
+
+            Change(false);
+        }
+
+        private void hatchedBackgroundToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _solidBgMode = !((ToolStripMenuItem)sender).Checked;
+            OnResizeHandler();
+        }
+
+        private void greenScreenVDUToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _greenScreen = ((ToolStripMenuItem)sender).Checked;
+            OnResizeHandler();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_changes && MessageBox.Show("You have unsaved changes. Continue with new?", "Unrecoverable Action", MessageBoxButtons.YesNo) == DialogResult.No)
+                return;
+
+            Array.Clear(_pixels, 0, _pixels.Length);
+            OnResizeHandler();
+
+            CurrentFile = null;
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_changes && MessageBox.Show("You have unsaved changes. Continue with quit?", "Unrecoverable Action", MessageBoxButtons.YesNo) == DialogResult.No)
+                return;
+
+            Close();
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_changes && MessageBox.Show("You have unsaved changes. Continue with clear?", "Unrecoverable Action", MessageBoxButtons.YesNo) == DialogResult.No)
+                return;
+
+            Array.Clear(_pixels, 0, _pixels.Length);
+            OnResizeHandler();
+        }
+
+        private void InvertPixels()
+        {
+            for (var y = 0; y < 48; ++y)
+            {
+                for (var x = 0; x < 128; ++x)
+                {
+                    _pixels[x, y] = !_pixels[x,y];
+                }
+            }
+        }
+
+        private void fillToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_changes && MessageBox.Show("You have unsaved changes. Continue with fill?", "Unrecoverable Action", MessageBoxButtons.YesNo) == DialogResult.No)
+                return;
+
+            Array.Clear(_pixels, 0, _pixels.Length);
+            InvertPixels();
+            OnResizeHandler();
+
+            Change(false);
+        }
+
+        private void invertToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            InvertPixels();
+            OnResizeHandler();
+        }
+    }
+}
